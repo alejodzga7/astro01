440d824af25b3a1b5d6d3216852f9b132bc5f605
ricar01010 <richarsonmartine@gmail.com>
2024-02-08 19:37:02 -0400

    primer commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+//export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://profwilliamarte.github.io',
+    base: '/astro01',
+    build: {
+       assets: 'astro' /// muy importante
+    }
+ });
